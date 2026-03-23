db1de0934b5f5b4c8fe204a37983e4e55d79d579
keilacortes <keilawonka4@gmail.com>
2026-03-23 13:16:55 -0300

    feat(cpp00): add ex00 - megaphone

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kqueiroz <kqueiroz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/23 12:12:15 by kqueiroz          #+#    #+#             */
+/*   Updated: 2026/03/23 12:56:13 by kqueiroz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+#include <string>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    for (int i = 1; i < argc; i++)
+    {
+        std::string arg = argv[i];
+        for (size_t j = 0; j < arg.size(); j++)
+            std::cout << static_cast<char>(std::toupper(arg[j]));
+        if (i < argc - 1)
+            std::cout << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
