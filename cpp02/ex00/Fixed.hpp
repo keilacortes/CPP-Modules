0a22e27aaa47bfdff21e4391a6e630231f2d61ba
Keila Queiroz Cortes <kqueiroz@c1r1p2.42sp.org.br>
2026-04-22 18:58:24 -0300

    initial files - cpp02

---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kqueiroz <kqueiroz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/22 17:38:15 by kqueiroz          #+#    #+#             */
+/*   Updated: 2026/04/22 17:45:22 by kqueiroz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+    public:
+        int fixed_val;
+        static const int bits = 8;
+
+    private:
+}
+
+#endif
